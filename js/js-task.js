34db4120cc3f5ab19f680bf3cf761e6b3b2eac36
PavlKud <144054118+PavlKud@users.noreply.github.com>
2023-12-19 22:37:40 +0100

    Update js-task.js

---
+++ js/js-task.js
@@ -0,0 +1,23 @@
+// const str = "Some World ktjn efwe";
+
+// const firstIndex = str.indexOf(" ");
+// const lastIndex = str.indexOf(" ", firstIndex + 1);
+
+// console.log(firstIndex)
+// console.log(str.slice(firstIndex + 1, lastIndex));
+// console.log(lastIndex)
+
+// const result = str.indexOf("5");
+
+// console.log(result)
+
+// const lastIndex = str.lastIndexOf(" ");
+// const firstIndex = str.lastIndexOf(" ", lastIndex - 1);
+
+// console.log(firstIndex)
+// console.log(str.slice(firstIndex + 1, lastIndex));
+// console.log(lastIndex)
+
+// const result = str.indexOf("5");
+
+
