911170d53a3858352b500fdfbe5b188025414f1f
PavlKud <144054118+PavlKud@users.noreply.github.com>
2023-12-20 23:07:18 +0100

    Update js-task.js

--- js/js-task.js
+++ js/js-task.js
@@ -21,3 +21,31 @@
 // const result = str.indexOf("5");
 
 
+// function getOrderQuantity(order) {
+//     return order.length;
+//   }
+
+//   console.log(getOrderQuantity(["apple", "peach", "pear", "banana"]))
+
+
+// function getLastElementMeta(array) {
+  
+//     const lastElementIndex = array.length - 1;
+//     const lastElementValue = array[lastElementIndex];
+  
+//     return[lastElementIndex, lastElementValue]
+    
+//   }
+
+//   console.log(getLastElementMeta(["apple", "peach", "pear", "banana"]))
+//   console.log(getLastElementMeta(["apple", "peach", "pear"]))
+//   console.log()
+//   console.log()
+
+function getExtremeElements(array) {
+    let firstElement = array[0];
+    let lastElement = array[array.length - 1];
+  
+    return[firstElement, lastElement]
+  }
+  console.log(getExtremeElements([1, 2, 3, 4, 5]))
